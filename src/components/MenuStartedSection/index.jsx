36cb1b2707432e3ee503b0d27f6ce992a8a3ec31
phwnganh <phuonganh25102003@gmail.com>
2026-03-15 22:33:02 +0700

    fix: padding responsive mobile

--- src/components/MenuStartedSection/index.jsx
+++ src/components/MenuStartedSection/index.jsx
@@ -2,7 +2,7 @@ import MenuStartedTitle from "./MenuStartedTitle.jsx";
 import MenuStartedList from "./MenuStartedList.jsx";
 const MenuStartedSection = () => {
     return (
-        <main className={"px-6 sm:px-0 sm:mt-11.75 md:mt-24.75 flex flex-col md:flex-row gap-10 sm:gap-16 md:gap-32"}>
+        <main className={"px-6 sm:px-16 md:px-0 sm:mt-11.75 md:mt-24.75 flex flex-col md:flex-row gap-10 sm:gap-16 md:gap-32"}>
             <MenuStartedTitle/>
             <MenuStartedList/>
         </main>

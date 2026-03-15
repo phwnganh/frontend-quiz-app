e188fe279b5e32fcae7066fff5c0102d47bb45d3
phwnganh <phuonganh25102003@gmail.com>
2026-03-15 23:05:09 +0700

    fix: background of the whole page

--- src/layouts/main.layout.jsx
+++ src/layouts/main.layout.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Outlet} from "react-router-dom";
 const MainLayout = () => {
     return (
-        <div className={"bg-[url(./assets/pattern-background-desktop-light.svg)] bg-grey-50"}>
+        <div className={"min-h-screen bg-[url(./assets/pattern-background-desktop-light.svg)] bg-grey-50 bg-no-repeat bg-top"}>
             <div className={"pt-4 sm:pt-24.25 max-w-290 mx-auto"}>
                 <Outlet/>
             </div>

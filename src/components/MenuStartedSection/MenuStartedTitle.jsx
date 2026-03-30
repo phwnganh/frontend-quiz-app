0c3c7fbe15e2cbde7964ab1eb78d5c2b6386f719
phwnganh <phuonganh25102003@gmail.com>
2026-03-30 20:36:16 +0700

    fix: html semantic bug

--- src/components/MenuStartedSection/MenuStartedTitle.jsx
+++ src/components/MenuStartedSection/MenuStartedTitle.jsx
@@ -7,7 +7,7 @@ const MenuStartedTitle = () => {
                 <h1 className={"text-preset-2-mobile-light sm:text-preset-2-light leading-preset-2 font-preset-2-light text-blue-900 dark:text-white"}>Welcome to the</h1>
                 <span className={"text-preset-2-mobile-medium sm:text-preset-2-mediun leading-preset-2 font-preset-2-medium text-blue-900 dark:text-white"}>Frontend Quiz!</span>
             </div>
-            <h2 className={"italic text-grey-500 dark:text-blue-300 text-preset-5-medium sm:text-preset-6 leading-preset-5 sm:leading-preset-6"}>Pick a subject to get started.</h2>
+            <span className={"italic text-grey-500 dark:text-blue-300 text-preset-5-medium sm:text-preset-6 leading-preset-5 sm:leading-preset-6"}>Pick a subject to get started.</span>
         </div>
     );
 };
--- src/components/uis/CustomSwitch.jsx
+++ src/components/uis/CustomSwitch.jsx
@@ -18,9 +18,9 @@ const CustomSwitch = () => {
 
 
     return (
-        <label htmlFor={"switch-mode"} className={"h-5 sm:h-7 w-8 sm:w-12 relative inline-block"}>
+        <label className={"h-5 sm:h-7 w-8 sm:w-12 relative inline-block"}>
             <input id={"switch-mode"} aria-label={"enable theme switch"} type={"checkbox"} checked={isDark} onChange={handleToggleTheme} className={"sr-only peer "}/>
-            <div className={"w-full h-full rounded-full bg-purple-600 peer-checked:bg-purple transition-colors"}></div>
+            <div className={"w-full h-full rounded-full bg-purple-600 peer-checked:bg-purple transition-colors"} aria-hidden={"true"}></div>
             <div className={"absolute top-0.75 left-0.75 h-3 w-3 sm:h-5 sm:w-5 rounded-full bg-white peer-checked:translate-x-full"}></div>
         </label>
     );
